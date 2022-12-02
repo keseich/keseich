443610b236a0ba94d058017da2a019ffa39c9243
florianthalmann <florianthalmann@bluewin.ch>
2022-12-02 17:32:23 +0900

    deploy

--- src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { ApiService } from './services/api.service';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {}
